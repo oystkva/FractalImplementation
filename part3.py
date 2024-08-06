b2697c0bf7be038519293cbe605efef235143c8c
oystkva <oystkva@stud.ntnu.no>
2024-08-06 10:07:21 +1000

    feat: :art: sierpinski_arrowhead_curve outlined

--- part3.py
+++ part3.py
@@ -2,3 +2,11 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
+def sierpinski_arrowhead_curve(order -> int, length -> int) -> None:
+    if order % 2 == 0:
+        # curve(order, length, +60)
+        return
+    else:
+        # curve(order, length, -60)
+        return
+
