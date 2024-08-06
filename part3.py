c838584c9c3fc005e550f28983d8ccba1fecdce5
oystkva <oystkva@stud.ntnu.no>
2024-08-06 10:33:31 +1000

    feat: :art: implemented curve function

--- part3.py
+++ part3.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
-def sierpinski_arrowhead_curve(order -> int, length -> int) -> None:
+def sierpinski_arrowhead_curve(order -> int, length -> float) -> None:
     if order % 2 == 0:
         # curve(order, length, +60)
         return
@@ -10,3 +10,15 @@ def sierpinski_arrowhead_curve(order -> int, length -> int) -> None:
         # curve(order, length, -60)
         return
 
+def curve(order -> int, length -> float, angle -> int) -> None:
+    if order == 0:
+        # draw(length)
+        return
+    else:
+        segment1 = curve(order - 1, length / 2, -angle)
+        turn1 = turn(angle)
+        segment2 = curve(order - 1, length / 2, angle)
+        turn2 = turn(angle)
+        segment3 = curve(order - 1, length / 2, -angle)
+
+        return torch.cat(segment1 @ turn1, segment2, segment3 @ turn2)
