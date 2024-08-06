8d683fef98277cf7b8e08ef90ea1281e7f827fda
oystkva <oystkva@stud.ntnu.no>
2024-08-06 10:40:05 +1000

    feat: :art:

--- part3.py
+++ part3.py
@@ -2,18 +2,16 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
-def sierpinski_arrowhead_curve(order -> int, length -> float) -> None:
+def sierpinski_arrowhead_curve(order: int, length: float) -> torch.Tensor:
     if order % 2 == 0:
-        # curve(order, length, +60)
-        return
+        return curve(order, length, +60)
     else:
-        # curve(order, length, -60)
-        return
+        return curve(order, length, -60)
 
-def curve(order -> int, length -> float, angle -> int) -> None:
+def curve(order: int, length: float, angle: int) -> torch.Tensor:
     if order == 0:
         # draw(length)
-        return
+        return torch.tensor([[length, 0]]).reshape(1, 2)
     else:
         segment1 = curve(order - 1, length / 2, -angle)
         turn1 = turn(angle)
@@ -21,4 +19,18 @@ def curve(order -> int, length -> float, angle -> int) -> None:
         turn2 = turn(angle)
         segment3 = curve(order - 1, length / 2, -angle)
 
-        return torch.cat(segment1 @ turn1, segment2, segment3 @ turn2)
+        return torch.cat((segment1 @ turn1, segment2, segment3 @ turn2))
+
+def turn(angle: int) -> torch.Tensor:
+    rad = torch.deg2rad(torch.tensor(angle, dtype=torch.float32))
+    return torch.tensor(
+        [[torch.cos(rad), -torch.sin(rad)],
+         [torch.sin(rad),  torch.cos(rad)]]
+        )
+
+
+
+
+order = 3
+length = 1.0
+points = sierpinski_arrowhead_curve(order, length)
